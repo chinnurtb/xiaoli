96c3b02f5bb51b9cf5270d256002ea1fcdaac02c
Ery Lee <ery.lee@gmail.com>
2012-09-03 18:42:50 +0800

    update fault

--- web/fault/views.py
+++ web/fault/views.py
@@ -8,16 +8,32 @@ from tango.login import login_required
 
 from tango.ui import menus, Menu
 
+from tango.ui import tables
+
 from tango.ui import Widget, add_widget
 
 from .models import Alarm
 
 faultview = Blueprint("fault", __name__, url_prefix='/fault')
 
+class AlarmTable(tables.Table):
+    check       = tables.CheckBoxColumn()
+    severity    = tables.Column(verbose_name=u'级别', orderable=True)
+    alarm_alias = tables.Column(verbose_name=u'告警名称', orderable=True)
+    node        = tables.Column(verbose_name=u'告警节点', accessor='node.alias', orderable=True)
+    summary     = tables.Column(verbose_name=u'告警详细')
+    last_occurrence = tables.Column(verbose_name=u'最后发生时间')
+
+    class Meta:
+        model = Alarm
+        per_page = 30
+        order_by = '-last_occurrence'
+
 @faultview.route('/')
 @login_required
 def index():
-    return render_template("/fault/index.html")
+    table = AlarmTable(Alarm.query, request)
+    return render_template("/fault/index.html", table = table)
 
 menus.append(Menu('fault', u'故障', '/fault'))
 
