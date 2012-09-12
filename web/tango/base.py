1936e9a4446128221dd8993b9d358126008095f6
Qian LinFeng <thewawar@gmail.com>
2012-09-12 09:51:17 +0800

    修复登录问题

--- web/tango/base.py
+++ web/tango/base.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+#coding=utf-8
 
 from flask import Flask, Blueprint
 
@@ -9,3 +11,26 @@ class Page(Blueprint):
     #TODO:
     pass
 
+class NestedDict(dict):
+
+    def __init__(self, request):
+        pass
+
+    def __getitem__(self, key):
+        value = None
+        if key not in self.keys():
+            value = NestedDict()
+            super(NestedDict, self).__setitem__(key, value)
+        else:
+            value = super(NestedDict, self).__getitem__(key)
+        return value
+
+    def __setitem__(self, key, value):
+        oldvalue = super(NestedDict, self).get(key, None)
+        if oldvalue:
+            if not isinstance(oldvalue, list):
+                oldvalue = [oldvalue]
+            oldvalue.append(value)
+            value = oldvalue
+        super(NestedDict, self).__setitem__(key, value)
+        
