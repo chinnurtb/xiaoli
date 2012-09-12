1da89e19c06ca61878a6244fe31dc1d1e7d2cc6a
Qian LinFeng <thewawar@gmail.com>
2012-09-12 13:09:36 +0800

    微调 NestedDict, 并添加一些注释

--- web/tango/base.py
+++ web/tango/base.py
@@ -12,15 +12,20 @@ class Page(Blueprint):
     pass
 
 class NestedDict(dict):
-
-    def __init__(self, request=None):
+    ''' 接收一个 flask.Request 对象, 提取其中的所有参数, 并生成一个嵌套的字典
+    具有相同 key 的 values 会得到一个由 values 组成的列表(list)
+    '''
+    
+    def __init__(self, request):
         if request is None:
             super(NestedDict, self).__init__()
             return
         kv_lists = request.values.lists()
         for k, v in kv_lists:
             if k == 'csrf_token' or v == '': continue
-            k_chains = k.split('.')
+            # key 的层级用 '.' 分割
+            # 例如, 给出 a.b.c=3 ,则会得到 {'a':{'b':{'c':'3'}}}
+            k_chains = k.split('.') 
             cur_level = self
             while len(k_chains) > 1:
                 cur_level = cur_level[k_chains.pop(0)]
@@ -33,7 +38,7 @@ class NestedDict(dict):
     def __getitem__(self, key):
         value = None
         if key not in self.keys():
-            value = NestedDict()
+            value = NestedDict(None)
             super(NestedDict, self).__setitem__(key, value)
         else:
             value = super(NestedDict, self).__getitem__(key)
