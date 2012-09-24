5aba411672d1ddbe880e7d98fd7b1ae3a950e00b
Ery Lee <ery.lee@gmail.com>
2012-09-24 09:29:57 +0800

    fix table -> tables

--- web/tango/views.py
+++ web/tango/views.py
@@ -10,7 +10,7 @@ from tango.ui import tables
 
 class SettingTable(tables.Table):
     
-    alias   = table.Column(verbose_name='名称', endpoint='settings_edit')
+    alias   = tables.Column(verbose_name='名称', endpoint='settings_edit')
 
 
 # ==============================================================================
