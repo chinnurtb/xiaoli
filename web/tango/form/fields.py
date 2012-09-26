317869f68a4421bc39399320574a5972976d31dc
wanghao <wangh@opengoss.com>
2012-09-26 15:09:30 +0800

    fix

--- web/tango/form/fields.py
+++ web/tango/form/fields.py
@@ -16,14 +16,30 @@ class SelectFieldPro(SelectField):
 class AreaSelectField(Field):
     widget = AreaSelectWidget()
 
+    def __init__(self, label=None, select_mode=2, **kwargs):
+        super(AreaSelectField, self).__init__(label,**kwargs)
+        self.select_mode = select_mode
+
+    def process(self, formdata, data=object()):
+        ori_name = self.name
+        self.name = ori_name + '_selected'
+        Field.process(self, formdata, data)
+        self.name = ori_name
+
     def _value(self):
         if self.data:
             return u', '.join(self.data)
         else:
             return u''
 
+    def populate_obj(self, obj, name):
+        data = self.data
+        if self.select_mode == 1:
+            data = "".join(self.data)
+        setattr(obj, name, data)
+
     def process_formdata(self, valuelist):
-        if valuelist:
+        if valuelist and valuelist[0]:
             self.data = [x.strip() for x in valuelist[0].split(',')]
         else:
             self.data = []
