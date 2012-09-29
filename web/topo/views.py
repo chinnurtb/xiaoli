1745759e75c92054f188dccad8e9c29fead2d97d
Qian LinFeng <thewawar@gmail.com>
2012-09-29 17:34:31 +0800

    topo: 1. select layout, 2. select level

--- web/topo/views.py
+++ web/topo/views.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, request, session, url_for, \
-    redirect, render_template, g, flash
-
+from flask import Blueprint, request, url_for, render_template
 import pydot
 from tango.ui import menus, Menu
 from nodes.models import Node, Area
@@ -36,61 +34,13 @@ node_edge_style = {
     'labelfontcolor' : '#009933',
 }
 
+
 @topoview.route('/topo/')
 def index():
-    node_url = lambda id : url_for('nodes.node_edit', id=id)
-
-    graph = pydot.Dot(graph_type='digraph')
-    def mk_areas(root):
-        if root is None:
-            return
-
-        area_root = pydot.Node('areas_%d' % root.id, label=root.name, **area_style)
-        graph.add_node(area_root)
-
-        node_nodes = Node.query.filter_by(area_id=root.id)
-        for node in node_nodes:
-            n_current = pydot.Node('nodes_%d' % node.id, label='%s\n%s' % (node.name, node.addr),
-                                   URL=node_url(node.id))
-            graph.add_node(n_current)
-            graph.add_edge(pydot.Edge(area_root, n_current))
-
-            if node.controller_id:
-                controller = Node.query.get(node.controller_id)
-                node_id = 'nodes_%d' % node.controller_id
-                n_controller =  graph.get_node(node_id)
-                if not n_controller:
-                    n_controller = pydot.Node('nodes_%d' % controller.id, label='%s\n%s' % (node.name, node.addr))
-                    graph.add_node(n_controller)
-                else:
-                    n_controller = n_controller[0]
-                graph.add_edge(pydot.Edge(n_controller, n_current))
-
-        for child in root.children:
-            node_child = mk_areas(child)
-            graph.add_node(node_child)
-            graph.add_edge(pydot.Edge(area_root, node_child, **area_edge_style))
-
-        return area_root
-
-    graph.set_node_defaults(**node_style)
-    graph.set_edge_defaults(**node_edge_style)
-
-    root_area = Area.query.get(1000)
-    mk_areas(root_area)
-    svg = unicode(graph.create(prog='dot', format='svg'), 'utf-8')
-    svg = svg[245:]
-        
-    return render_template("topo/index.html", svg=svg, menuid = 'topo')
-
-
-@topoview.route('/viewtopo/')
-def viewtopo():
     root_id = request.args.get('root_id', 1000, type=int)
     level = request.args.get('level', 3, type=int)
     prog = request.args.get('prog', 'dot')
-    area_url = lambda root_id : url_for('topo.viewtopo', root_id=root_id,
-                                        level=level, prog=prog)
+    area_url = lambda root_id : url_for('topo.index', root_id=root_id, level=level, prog=prog)
     node_url = lambda node_id : url_for('nodes.node_edit', id=node_id)
     
     graph = pydot.Dot(graph_type='digraph')
@@ -151,7 +101,56 @@ def viewtopo():
         base = base.parent
     svg = unicode(graph.create(prog=prog, format='svg'), 'utf-8')
     svg = svg[245:]
-    return render_template("topo/index.html", svg=svg, breadcrumb=breadcrumb)
+    return render_template("topo/index.html", svg=svg, breadcrumb=breadcrumb,
+                           root_id=root_id, level=level, prog=prog)
     
+    
+@topoview.route('/topo/view-all')
+def view_all():
+    node_url = lambda id : url_for('nodes.node_edit', id=id)
+
+    graph = pydot.Dot(graph_type='digraph')
+    def mk_areas(root):
+        if root is None:
+            return
+
+        area_root = pydot.Node('areas_%d' % root.id, label=root.name, **area_style)
+        graph.add_node(area_root)
+
+        node_nodes = Node.query.filter_by(area_id=root.id)
+        for node in node_nodes:
+            n_current = pydot.Node('nodes_%d' % node.id, label='%s\n%s' % (node.name, node.addr),
+                                   URL=node_url(node.id))
+            graph.add_node(n_current)
+            graph.add_edge(pydot.Edge(area_root, n_current))
+
+            if node.controller_id:
+                controller = Node.query.get(node.controller_id)
+                node_id = 'nodes_%d' % node.controller_id
+                n_controller =  graph.get_node(node_id)
+                if not n_controller:
+                    n_controller = pydot.Node('nodes_%d' % controller.id, label='%s\n%s' % (node.name, node.addr))
+                    graph.add_node(n_controller)
+                else:
+                    n_controller = n_controller[0]
+                graph.add_edge(pydot.Edge(n_controller, n_current))
+
+        for child in root.children:
+            node_child = mk_areas(child)
+            graph.add_node(node_child)
+            graph.add_edge(pydot.Edge(area_root, node_child, **area_edge_style))
+
+        return area_root
+
+    graph.set_node_defaults(**node_style)
+    graph.set_edge_defaults(**node_edge_style)
+
+    root_area = Area.query.get(1000)
+    mk_areas(root_area)
+    svg = unicode(graph.create(prog='dot', format='svg'), 'utf-8')
+    svg = svg[245:]
+        
+    return render_template("topo/index.html", svg=svg, menuid = 'topo')
+
     
 menus.append(Menu('topo', u'拓扑', '/topo'))
