1472760a181602e4a6a7cf48834ba85e86faca98
wanghao <wangh@opengoss.com>
2012-10-16 10:09:49 +0800

    nodes/show

--- web/tango/ui/charts/configs.py
+++ web/tango/ui/charts/configs.py
@@ -67,51 +67,53 @@ area_stacked = '''{
 '''
 
 pie_basic = '''{
-   chart: {
-       renderTo: 'container',
-       plotBackgroundColor: null,
-       plotBorderWidth: null,
-       plotShadow: false
-   },
-   title: {
-       text: 'Browser market shares at a specific website, 2010'
-   },
-   tooltip: {
-       pointFormat: '{series.name}: <b>{point.percentage}%</b>',
-       percentageDecimals: 1
-   },
-   plotOptions: {
-       pie: {
-           allowPointSelect: true,
-           cursor: 'pointer',
-           events: {
+    chart: {
+        renderTo: 'container',
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false
+    },
+    title: {
+        text: 'Browser market shares at a specific website, 2010'
+    },
+    tooltip: {
+        formatter: "function() {  return ''+this.point.name +': '+ this.y;}"
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: false
+            },
+            showInLegend: true,
+            events: {
                click: "function(){location.href = 'http://www.google.com';}",
-           },
-           dataLabels: {
-               enabled: true,
-               color: '#000000',
-               connectorColor: '#000000',
-               formatter: "function() {return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';}"
            }
-       }
-   },
-   series: [{
-       type: 'pie',
-       name: 'Browser share',
-       data: [
-           ['Firefox',   45.0],
-           ['IE',       26.8],
-           {
-               name: 'Chrome',
-               y: 12.8,
-               sliced: true,
-               selected: true
-           },
-           ['Safari',    8.5],
-           ['Opera',     6.2],
-           ['Others',   0.7]
-       ]
-   }]
+        }
+    },
+    series: [{
+        type: 'pie',
+        data: [{
+            name: '紧急',
+            y: 0
+        }, {
+            name: '重要',
+            y: 0
+        }, {
+            name: '次要',
+            y: 0
+        }, {
+            name: '警告',
+            y: 0
+        }, {
+            name: '事件',
+            y: 0
+        }, {
+            name: '清除',
+            y: 19
+        }]
+    }]
 }
 '''
 
