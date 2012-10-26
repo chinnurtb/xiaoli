ce78dba5d073fc536be6a930045fd1dcd6551dc4
Qian LinFeng <thewawar@gmail.com>
2012-10-26 18:15:42 +0800

    删除所有项

--- web/static/js/base.js
+++ web/static/js/base.js
@@ -1,9 +1,25 @@
 $(function() {
-	var attrs = $(".desc .attrs");
-	$(".desc .trigger").click(function() {
-		attrs.toggleClass("hide");
-		$(this).toggleClass("down");
-		return false;
-	});
+  var attrs = $(".desc .attrs");
+  $(".desc .trigger").click(function() {
+    attrs.toggleClass("hide");
+    $(this).toggleClass("down");
+    return false;
+  });
 });
+
+
+$(function(){
+  $('#multi-delete-btn').click(function() {
+    if ($('table tr th [type=checkbox]:checked').length > 0) {
+      if(confirm('删除所有?')){
+        $('#table-form').submit();
+      }
+    } else {
+      alert('请选择要删除的项!')
+      return false;
+    }
+  });
+})
+
+
 
