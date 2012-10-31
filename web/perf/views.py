76ab50ac55f51ee33282f4574f83336be3d8ee46
Qian LinFeng <thewawar@gmail.com>
2012-10-31 14:25:33 +0800

    perf 中的搜索

--- web/perf/views.py
+++ web/perf/views.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-
+from datetime import datetime, timedelta
 from flask import Blueprint, request, url_for, \
     redirect, render_template, flash, json
 
@@ -14,10 +14,32 @@ from .tables import NodePerfTable
 from .forms import PerfFilterForm, pull_intervals, model_choices
 from .tables import *
 
-from .forms import PerfFilterForm
-
 perfview = Blueprint('perf', __name__, url_prefix="/perf")
 
+
+@perfview.context_processor
+def inject_navid():
+    return dict(navid = 'perf')
+
+    
+@perfview.route('/refresh/intervals')
+def ajax_refresh_intervals():
+    """ 刷新表单中的时间选框中的选项 """
+    key = request.args.get('key', '')
+    res = pull_intervals(key)
+    return json.dumps(res)
+
+    
+@perfview.route('/refresh/models')
+def ajax_refresh_models():
+    """ 刷新搜索表单中的 (厂商/型号) 选框中的选项 """
+    vendors = dict(request.values.lists()).get('vendors[]', [])
+    models = apply(model_choices(vendors))
+    res = [[str(model.id), model.alias] for model in models]
+    return json.dumps(res)
+    
+
+        
 CONFIG = {
     'node_ping': (u'PING时延', NodePerf, PingTable, ['pingrta', 'pingrtmax', 'pingrtmin']),
     'node_cpumem': (u'CPU内存', NodePerf, CpuMemTable, []),
@@ -26,18 +48,44 @@ CONFIG = {
     'olt_ping': (u'PING时延', NodePerf, PingTable, ['pingrta', 'pingrtmax', 'pingrtmin'])
 }
 
-@perfview.context_processor
-def inject_navid():
-    return dict(navid = 'perf')
-
+@perfview.route('/do-db')
+def add_time():
+    from random import Random
+    import calendar
+    rand = Random()
+    num = request.args.get('num', 50, type=int)
+        
+    for i in range(num):
+        year = 2012
+        month = rand.randint(9, 10)
+        monthrange = calendar.monthrange(year, month)[1]
+        day = rand.randint(1, monthrange)
+        hour = rand.randint(0, 23)
+        minute = rand.randint(0, 59)
+        dt = datetime(year, month, day, hour, minute)
+        node_perf = NodePerf()
+        node_perf.sampletime = dt
+        node_perf.sampleyear = year
+        node_perf.samplemonth = month
+        node_perf.sampleday = day
+        node_perf.sampleweekday = dt.weekday()
+        node_perf.samplehour = hour
+        node_perf.nodeid = 104
+        db.session.add(node_perf)
+    db.session.commit()
+    return 'OK: ' + str(num)
 
     
 @perfview.route('/node/<name>')
 def node(name):
     menuid = 'node_' + name
     title, model, tblcls, metrics = CONFIG[menuid]
-    table = make_table(model.query, tblcls)
     form = PerfFilterForm(formdata=request.args)
+    form.refresh_choices(request.args)
+
+    query = model.query
+    query = form.filter(model, query)
+    table = make_table(query, tblcls)
     return render_template('/perf/node/index.html',
         menuid = menuid, title = title,
         name = name, filterForm = form,
@@ -87,33 +135,6 @@ def eoc(name):
         name = name, filterForm = form,
         table = table)
 
-@perfview.route('/switches')
-def switches():
-    form = PerfFilterForm(formdata=request.args)
-    form.intervals.choices = pull_intervals(request.args.get('sampletime'))
-    form.models.query_factory = model_choices(request.args.get('vendors'))
-    q = NodePerf.query
-    t = make_table(q, NodePerfTable)
-    return render_template('/perf/switches/index.html',
-        filterForm = form, table=t)
-
-    
-@perfview.route('/switches/intervals')
-def switches_intervals():
-    """ 给搜索表单中的时间选框提供 ajax 服务 """
-    key = request.args.get('key', '')
-    res = pull_intervals(key)
-    return json.dumps(res)
-
-    
-@perfview.route('/switches/models')
-def switches_models():
-    """ 给搜索表单中的 (厂商/型号) 选框提供 ajax 服务 """
-    vendors = dict(request.values.lists()).get('vendors[]', [])
-    models = apply(model_choices(vendors))
-    res = [[str(model.id), model.alias] for model in models]
-    return json.dumps(res)
-    
     
 @perfview.route('/olts/')
 def olts():
