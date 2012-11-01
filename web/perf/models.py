7e8b30e5d09d1573bd1acfb769a2738da50f769b
Ery Lee <ery.lee@gmail.com>
2012-11-01 15:09:42 +0800

    update perf model

--- web/perf/models.py
+++ web/perf/models.py
@@ -2,92 +2,199 @@
 
 from tango import db
 
-class NodePerf(db.Model):
-    '''节点性能'''
+from sqlalchemy.ext.declarative import declared_attr 
+
+__all__ = ['PingPerf',
+           'CpuMemPerf',
+           'BoardPerf',
+           'IntfTrafficPerf',
+           'IntfUsagePerf',
+           'PonPowerPerf',
+           'PonTrafficPerf']
+
+class PerfMixin(object):
+
+    id              = db.Column(db.Integer, primary_key=True)
+
+    @declared_attr 
+    def nodeid(cls):
+        return db.Column(db.Integer, db.ForeignKey("nodes.id"))
+
+    @declared_attr 
+    def node(cls):
+        return db.relation('Node')
+
+    sampleyear      = db.Column(db.Integer)
+    samplemonth     = db.Column(db.Integer)
+    sampleday       = db.Column(db.Integer)
+    sampleweekday   = db.Column(db.Integer)
+    samplehour      = db.Column(db.Integer)
+    sampletime      = db.Column(db.DateTime)
+
+class PingPerf(db.Model, PerfMixin):
+
+    '''节点PING指标'''
     
-    __tablename__ = 'perf_node'
+    __tablename__ = 'perf_ping'
 
-    id            = db.Column(db.Integer, primary_key=True)
-    nodeid        = db.Column(db.Integer, db.ForeignKey("nodes.id"))
-    sampletime    = db.Column(db.DateTime)
-    sampleyear    = db.Column(db.Integer)
-    samplemonth   = db.Column(db.Integer)
-    sampleday     = db.Column(db.Integer)
-    sampleweekday = db.Column(db.Integer)
-    samplehour    = db.Column(db.Integer)
-    pingrta       = db.Column(db.Float)
-    pingrtmax     = db.Column(db.Float)
-    pingrtmin     = db.Column(db.Float)
-    pingloss      = db.Column(db.Integer)
+    rta     = db.Column(db.Float)
+    rtmax   = db.Column(db.Float)
+    rtmin   = db.Column(db.Float)
+    lossavg = db.Column(db.Float)
+    lossmax = db.Column(db.Float)
+    stdevavg = db.Column(db.Float)
+    stdevmax = db.Column(db.Float)
 
-    node        = db.relation('Node')
+class CpuMemPerf(db.Model, PerfMixin):
+    
+    '''节点CPU内存指标'''
 
-class BoardPerf(db.Model):
-    '''版卡性能'''
+    __tablename__ = 'perf_cpumem'
+    cpuavg      = db.Column(db.Float)
+    cpumax      = db.Column(db.Float) 
+    memavg      = db.Column(db.Float) 
+    memmax      = db.Column(db.Float) 
+    tempavg     = db.Column(db.Float) 
+    tempmax     = db.Column(db.Float) 
+    powerstate  = db.Column(db.Integer)
+    fanstate    = db.Column(db.Integer)
+
+class BoardPerf(db.Model, PerfMixin):
+
+    '''版卡指标'''
     
     __tablename__ = 'perf_board'
 
-    id          = db.Column(db.Integer, primary_key=True)
-    nodeid      = db.Column(db.Integer, db.ForeignKey("nodes.id"))
-    sampletime  = db.Column(db.DateTime)
-    cpuavg      = db.Column(db.Integer)
-    cpumax      = db.Column(db.Integer) 
-    memavg      = db.Column(db.Integer) 
-    memmax      = db.Column(db.Integer) 
-    tempavg     = db.Column(db.Integer) 
-    tempmax     = db.Column(db.Integer) 
+    boardidx    = db.Column(db.Integer)
+    boardtype   = db.Column(db.Integer)
+    cpuavg      = db.Column(db.Float)
+    cpumax      = db.Column(db.Float) 
+    memavg      = db.Column(db.Float) 
+    memmax      = db.Column(db.Float) 
+    tempavg     = db.Column(db.Float) 
+    tempmax     = db.Column(db.Float) 
 
-    node        = db.relation('Node')
+class IntfUsagePerf(db.Model, PerfMixin):
+
+    '''接口占用指标'''
+
+    __tablename__ = 'perf_intfusage'
+
+    intftype    = db.Column(db.Integer)
+
+    intftotal   = db.Column(db.Integer)
+    intfused    = db.Column(db.Integer)
+    intfusage   = db.Column(db.Float)
     
 
-class PortPerf(db.Model):
-    '''端口性能'''
+class IntfTrafficPerf(db.Model, PerfMixin):
 
-    __tablename__ = 'perf_port'
+    '''接口流量流速指标'''
 
-    id          = db.Column(db.Integer, primary_key=True)
-    nodeid      = db.Column(db.Integer, db.ForeignKey("nodes.id"))
-    portid      = db.Column(db.Integer, db.ForeignKey("ports.id"))
+    __tablename__ = 'perf_intftraffic'
+
+    intfidx     = db.Column(db.Integer)
+    intftype    = db.Column(db.Integer)
+    ifspeed     = db.Column(db.Integer)
     
-    sampletime    = db.Column(db.DateTime)
-    sampleyear    = db.Column(db.Integer)
-    samplemonth   = db.Column(db.Integer)
-    sampleday     = db.Column(db.Integer)
-    sampleweekday = db.Column(db.Integer)
-    samplehour    = db.Column(db.Integer)
-
     inoctets    = db.Column(db.Float)
     inoctetsmax = db.Column(db.Float)
     inpkts      = db.Column(db.Integer)
     inpktsmax   = db.Column(db.Integer)
     inucastpkts = db.Column(db.Integer)
 
-    inmulticastpkts = db.Column(db.Integer)
-    inbroadcastpkts = db.Column(db.Integer)
-    innucastpkts    = db.Column(db.Integer)
-    indiscards      = db.Column(db.Integer)
-    inerrors        = db.Column(db.Integer)
-    inunknownprotos = db.Column(db.Integer)
+    inmulticastpkts = db.Column(db.Float)
+    inbroadcastpkts = db.Column(db.Float)
+    innucastpkts    = db.Column(db.Float)
+    indiscards      = db.Column(db.Float)
+    inerrors        = db.Column(db.Float)
+    inunknownprotos = db.Column(db.Float)
 
     outoctets       = db.Column(db.Float)
     outoctetsmax    = db.Column(db.Float)
-    outpkts         = db.Column(db.Integer) 
-    outpktsmax      = db.Column(db.Integer) 
-    outucastpkts    = db.Column(db.Integer) 
-    outmulticastpkts= db.Column(db.Integer)
-    outbroadcastpkts= db.Column(db.Integer) 
-    outnucastpkts   = db.Column(db.Integer) 
-    outdiscards     = db.Column(db.Integer) 
-    outerrors       = db.Column(db.Integer)
+    outpkts         = db.Column(db.Float) 
+    outpktsmax      = db.Column(db.Float) 
+    outucastpkts    = db.Column(db.Float) 
+    outmulticastpkts= db.Column(db.Float)
+    outbroadcastpkts= db.Column(db.Float) 
+    outnucastpkts   = db.Column(db.Float) 
+    outdiscards     = db.Column(db.Float) 
+    outerrors       = db.Column(db.Float)
 
-    node            = db.relation('Node')
-    port            = db.relation('Port')
+    #====================================
+    # 定义计算指标
+    #====================================
+    @property #接收流量
+    def inoctetstotal(self):
+        return self.inoctets * 3600 / 8
 
+    @property #接收带宽利用率
+    def inbwusage(self):
+        return (self.inoctets / self.ifspeed) * 100
 
-    #TODO: 定义计算函数
-    #接收流量
-    #接收带宽利用率
-    #接收带宽利用率峰值
-    #发送流量
-    #发送带宽利用率
-    #发送带宽利用率峰值
+    @property #接收带宽利用率峰值
+    def inbwusagemax(self):
+        return (self.inoctetsmax / self.ifspeed) * 100
+
+    @property #发送流量
+    def outoctetstotal(self):
+        return outoctets * 3600 / 8
+
+    @property #发送带宽利用率
+    def outbwusage(self):
+        return (self.outoctets / self.ifspeed) * 100
+
+    @property #发送带宽利用率峰值
+    def outbwusagemax(self):
+        return (self.outoctetsmax / self.ifspeed) * 100
+
+class PonTrafficPerf(db.Model, PerfMixin):
+
+    __tablename__ = 'perf_pontraffic'
+
+    ponidx      = db.Column(db.Integer)
+    rx64bytes   = db.Column(db.Float) 
+    tx64bytes   = db.Column(db.Float) 
+    rx128bytes  = db.Column(db.Float)
+    tx128bytes  = db.Column(db.Float)
+    rx256bytes  = db.Column(db.Float)
+    tx256bytes  = db.Column(db.Float)
+    rx512bytes  = db.Column(db.Float)
+    tx512bytes  = db.Column(db.Float)
+    rx1024bytes = db.Column(db.Float)
+    tx1024bytes = db.Column(db.Float)
+    rx1518bytes = db.Column(db.Float)
+    tx1518bytes = db.Column(db.Float)
+    rxover1518bytes = db.Column(db.Float) 
+    txover1518bytes = db.Column(db.Float) 
+        
+class PonPowerPerf(db.Model, PerfMixin):
+
+    __tablename__ = 'perf_ponpower'
+    
+    ponidx      = db.Column(db.Integer)
+    rxpoweravg  = db.Column(db.Float) 
+    rxpowermax  = db.Column(db.Float) 
+    rxpowermin  = db.Column(db.Float) 
+    txpoweravg  = db.Column(db.Float) 
+    txpowermax  = db.Column(db.Float) 
+    txpowermin  = db.Column(db.Float) 
+    currentavg  = db.Column(db.Float) 
+    currentmax  = db.Column(db.Float) 
+    currentmin  = db.Column(db.Float) 
+    voltageavg  = db.Column(db.Float) 
+    voltagemax  = db.Column(db.Float) 
+    voltagemin  = db.Column(db.Float) 
+
+    tempavg     = db.Column(db.Float) 
+    tempmax     = db.Column(db.Float) 
+    tempmin     = db.Column(db.Float) 
+
+class PonUsagePerf(db.Model, PerfMixin):
+
+    __tablename__ = 'perf_ponusage'
+
+    pontotal    = db.Column(db.Integer)
+    ponused     = db.Column(db.Integer)
+    ponusage    = db.Column(db.Float)
+
