4decb940f6892bbe401f8382460253c72992a229
wanghao <wangh@opengoss.com>
2012-12-03 11:19:57 +0800

    license 移入 tango/license

--- web/tango/license/decrypt_license.py
+++ web/tango/license/decrypt_license.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 from pyDes import CBC, PAD_PKCS5, triple_des
 
 def decrypt_license(des_key):
@@ -8,7 +9,7 @@ def decrypt_license(des_key):
     else:
         des_key = des_key[0:16]
     k = triple_des(des_key, CBC, "opengoss",pad=None, padmode=PAD_PKCS5)
-    with open("LICENSE."+ suffix, 'rb') as file:
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "LICENSE."+ suffix), 'rb') as file:
         en_data = ''.join(file.readlines())
     de_data = k.decrypt(en_data)
     license_dict = {}
